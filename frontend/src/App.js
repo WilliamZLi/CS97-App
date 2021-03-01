215557fb87244f722585cdf48dc749417b360022
Jaden Nguyen <jadennguyen@g.ucla.edu>
2021-02-28 16:24:54 -0800

    Added styling to login page (home component)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import axios from 'axios';
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
@@ -17,14 +17,15 @@ import Register from "./components/register.component";
 import Login from "./components/login.component";
 import NotFound from "./components/notfound.component";
 import Home from "./components/home.component";
-import Search from "./components/search.component";   // import Search
+import Search from "./components/search.component"; // import Search
 import Friends from "./components/show-friend.component";
 axios.defaults.withCredentials = true;
 
 function App() {
-  return (<Router>
-    <div className="App">
-      <header className="App-header">
+  return (
+    <Router>
+      <div className="App">
+        {/* <header className="App-header">
         <Navbar bg="dark" variant="dark">
           <Container>
 
@@ -74,29 +75,30 @@ function App() {
 
           </Container>
         </Navbar>
-      </header>
+      </header> */}
 
-      <Container>
-        <Row>
-          <Col md={12}>
-            <div className="wrapper">
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path="/create-obj" component={CreateObj} />
-                <Route exact path="/list-obj" component={ListObj} />
-                <Route exact path="/logout" component={Logout} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/search" component={Search} />
-                <Route exact path="/friends" component={Friends} />
-                <Route path="" component={NotFound} />
-              </Switch>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  </Router>);
+        {/* <Container> */}
+        {/* <Row> */}
+        {/* <Col md={12}> */}
+        {/* <div className="wrapper"> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/create-obj" component={CreateObj} />
+          <Route exact path="/list-obj" component={ListObj} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/search" component={Search} />
+          <Route exact path="/friends" component={Friends} />
+          <Route path="" component={NotFound} />
+        </Switch>
+        {/* </div> */}
+        {/* </Col> */}
+        {/* </Row> */}
+        {/* </Container> */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- frontend/src/components/home.component.js
+++ frontend/src/components/home.component.js
@@ -1,62 +1,70 @@
 import React, { Component } from "react";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button';
-import axios from 'axios';
+
+import "./home.css";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 import { Redirect } from "react-router-dom";
 axios.defaults.withCredentials = true;
+
 export default class Login extends Component {
-    constructor(props) {
-        super(props)
-        this.onSubmit = this.onSubmit.bind(this);
-        this.onSubmit2 = this.onSubmit2.bind(this);
-        // Setting up functions - set 'this' context to this class
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onSubmit2 = this.onSubmit2.bind(this);
+    // Setting up functions - set 'this' context to this class
 
-        // Setting up state
-        this.state = {
-            redirect: null,
-            loading_done: false
-        }
-        axios.post('http://localhost:5000/auth/logged')
-        .then(res => {
-            console.log(res)
-            this.setState({ redirect: '/create-obj', loading_done: true })
-        })
-        .catch(err => {
-            console.log(err)
-            this.setState({loading_done: true})
-        })
-    }
+    // Setting up state
+    this.state = {
+      redirect: null,
+      loading_done: false,
+    };
+    axios
+      .post("http://localhost:5000/auth/logged")
+      .then((res) => {
+        console.log(res);
+        this.setState({ redirect: "/create-obj", loading_done: true });
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({ loading_done: true });
+      });
+  }
 
-    onSubmit(e) {
-        e.preventDefault()
-        this.setState({ redirect: '/login' })
-    }
-    onSubmit2(e) {
-        e.preventDefault()
-        this.setState({ redirect: '/register' })
-    }
+  onSubmit(e) {
+    e.preventDefault();
+    this.setState({ redirect: "/login" });
+  }
+  onSubmit2(e) {
+    e.preventDefault();
+    this.setState({ redirect: "/register" });
+  }
 
-    render() {
-        if (this.state.redirect) {
-            return <Redirect to={this.state.redirect} />
-        }
-        else if (this.state.loading_done) {
-            return (<div className="form-wrapper">
-                <Form onSubmit={this.onSubmit}>
-                    <Button variant="danger" size="lg" block="block" type="submit">
-                        Login
-        </Button>
-                </Form>
-                <Form onSubmit={this.onSubmit2}>
-                    <Button variant="danger" size="lg" block="block" type="submit">
-                        Register
-        </Button>
-                </Form>
-            </div>);
-        }
-        else {
-            return(<div>Loading...</div>)
-        }
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    } else if (this.state.loading_done) {
+      return (
+        <div className="form-wrapper">
+          <div>
+            <Form onSubmit={this.onSubmit}>
+              <Button size="lg" block="block" type="submit" className="button">
+                Login
+              </Button>
+            </Form>
+          </div>
+          <div>
+            <Form onSubmit={this.onSubmit2}>
+              <Button size="lg" block="block" type="submit" className="button">
+                Register
+              </Button>
+            </Form>
+          </div>
+        </div>
+      );
+    } else {
+      return <div>Loading...</div>;
     }
+  }
 }
-
