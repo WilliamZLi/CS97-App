cee58175521cf1fc589b6c64f1b00bcfd62be11e
williamzli <williamzli@g.ucla.edu>
2021-03-07 22:01:31 -0800

    remove repreciated files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import userHome from "./components/user-home.component";
 import CreateObj from "./components/create-obj.component";
 import MyProfile from "./components/list-obj.component";
 import Logout from "./components/logout.component";
@@ -36,7 +30,6 @@ function App() {
         {/* <div className="wrapper"> */}
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={userHome} />
           <Route exact path="/create-obj" component={CreateObj} />
           <Route exact path="/logout" component={Logout} />
           <Route exact path="/login" component={Login} />
